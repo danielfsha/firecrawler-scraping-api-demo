ab0b53c7c85596399192399a0e2b0502e0a312e5
Daniel <developedbydanielfisseha@gmail.com>
2025-04-05 22:12:47 -0700

    added first commit

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import express from 'express'
+import FirecrawlApp from '@mendable/firecrawl-js';
+
+const PORT = process.env.PORT || 3000
+const app = express()
+app.use(express.json())
+
+const FIRECRAWL_API_KEY = process.env.FIRECRAWL_API_KEY
+
+if(!FIRECRAWL_API_KEY) {
+    throw new Error("FIRECRAWL API KEY isn't defined'")
+}
+
+const firecrawl = new FirecrawlApp({
+    apiKey: process.env.FIRECRAWL_API_KEY
+})
+
+
+
+app.post('/scrape', async (req, res) => {
+    const { URL } = req.body
+
+    try {
+        const crawlResponse = await firecrawl.scrapeUrl(URL, {
+            formats: ['markdown', 'html', 'screenshot@fullPage'],
+        })
+
+        if (!crawlResponse.success) {
+            throw new Error(`Crawl failed: ${crawlResponse.error}`)
+        }
+
+        // console.log(crawlResponse)
+        res.status(200).json({crawlResponse: crawlResponse})
+    } catch (error) {
+        throw new Error(`Crawl failed: ${error}`)
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
